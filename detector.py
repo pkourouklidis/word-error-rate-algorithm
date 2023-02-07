a414284cf2f30819cacda6f48cf2350222ead164
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-02-07 20:15:56 +0000

    initial commit

---
+++ detector.py
@@ -0,0 +1,15 @@
+import jiwer
+
+def detector(trainSet, liveSet, parameters):
+    firstColumn = liveSet.axes[1][0]
+    secondColumn = liveSet.axes[1][1]
+    transformation = jiwer.Compose([
+        jiwer.ToLowerCase(),
+        jiwer.RemoveWhiteSpace(replace_by_space=True),
+        jiwer.RemovePunctuation(),
+        jiwer.ReduceToListOfListOfWords()
+    ])
+
+    wer = jiwer.wer(liveSet[firstColumn], liveSet[secondColumn], truth_transform=transformation, hypothesis_transform=transformation)
+    threshold = float (parameters.get("threshold", 0.3))
+    return int(wer>threshold), wer
